281438b672cfff83fb636d6976d358d06bc0e1b2
Victor Valls <42036246+vvalls@users.noreply.github.com>
2020-09-02 11:26:33 +0200

    Test added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,10 @@
 using Test
 
 using BirkhoffDecomposition
+
+# Generate a random doubly stochastic matrix (n is the dimension)
+n  = 3;
+x  = randomDoublyStochasticMatrix(n);
+
+# Find decomposition
+P, w = birkdecomp(x);
